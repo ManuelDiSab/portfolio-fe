2b12258b332483006febbac3a52638e240f2021f
Manuel <manuelds0094@gmail.com>
2025-11-29 12:56:35 +0100

    Aggiunta pagina 404 e aggiustamento navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/router';
 import { HomepageComponent } from './_pagine/homepage/homepage.component';
 
 export const routes: Routes = [
-    { path:'', component:HomepageComponent}
+    { path: '', component: HomepageComponent },
+    { path: '**', loadComponent: () => import('./_pagine/notfound/notfound.component')
+      .then(m => m.NotfoundComponent) }
 ];
